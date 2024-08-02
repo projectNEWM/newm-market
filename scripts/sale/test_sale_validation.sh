d32a3f585eaea2341f797f0d40d5247cf1492eee
logicalmechanism <logical.mechanism@protonmail.com>
2024-08-02 12:33:08 -0700

    if margin is zero then dont use the vault or oracle works as expected

--- scripts/sale/test_sale_validation.sh
+++ scripts/sale/test_sale_validation.sh
@@ -1,1 +1,13 @@
-python3 -c "import sys, json; sys.path.append('../py/'); from tx_simulation import from_file; exe_units=from_file('../tmp/tx-purchase.draft', False, debug=True);print(json.dumps(exe_units))" > ../data/exe_units.json
+#!/bin/bash
+
+# Initialize the base command
+cmd="cardano-cli"
+
+# Append to the command
+cmd+=" --version"
+
+# Execute the command
+eval $cmd
+
+
+# python3 -c "import sys, json; sys.path.append('../py/'); from tx_simulation import from_file; exe_units=from_file('../tmp/tx-purchase.draft', False, debug=True);print(json.dumps(exe_units))" > ../data/exe_units.json
