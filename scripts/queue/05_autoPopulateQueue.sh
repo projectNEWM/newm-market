174deb6e37fbb397115ecbc4cd339fae406e2291
logicalmechanism <logical.mechanism@protonmail.com>
2024-08-08 15:24:32 -0700

    auto fill for a single sale is updated

--- scripts/queue/05_autoPopulateQueue.sh
+++ scripts/queue/05_autoPopulateQueue.sh
@@ -1,179 +1,156 @@
 #!/usr/bin/env bash
 set -e
 
-MAX_NUMBER_OF_TX=250
-
 export CARDANO_NODE_SOCKET_PATH=$(cat ../data/path_to_socket.sh)
 cli=$(cat ../data/path_to_cli.sh)
 network=$(cat ../data/network.sh)
 
+# staking contract
 stake_script_path="../../contracts/stake_contract.plutus"
+
+# bundle sale contract
 queue_script_path="../../contracts/queue_contract.plutus"
 script_address=$(${cli} address build --payment-script-file ${queue_script_path} --stake-script-file ${stake_script_path} ${network})
 
-max_bundle_size=$(jq -r '.fields[3].int' ../data/sale/sale-datum.json)
-
-number_of_bundles=$(python3 -c "import random; print(random.randint(1, ${max_bundle_size}))")
-echo Buying ${number_of_bundles} Bundles
-buyer_number=$(python3 -c "import random; print(random.randint(1, 2))")
-
-issuer_path="issuer-wallet"
-issuer_address=$(cat ../wallets/${issuer_path}/payment.addr)
-
-# issuer order for
-buyer_path="buyer${buyer_number}-wallet"
+# collat, buyer, reference
+buyer_path="buyer1-wallet"
 buyer_address=$(cat ../wallets/${buyer_path}/payment.addr)
 buyer_pkh=$(${cli} address key-hash --payment-verification-key-file ../wallets/${buyer_path}/payment.vkey)
+
+max_bundle_size=$(jq -r '.fields[3].int' ../data/sale/sale-datum.json)
+bundleSize=$(python3 -c "from random import randint; print(randint(1, 4000000))")
+
 
 variable=${buyer_pkh}; jq --arg variable "$variable" '.fields[0].fields[0].bytes=$variable' ../data/queue/queue-datum.json > ../data/queue/queue-datum-new.json
 mv ../data/queue/queue-datum-new.json ../data/queue/queue-datum.json
 
-variable=${number_of_bundles}; jq --argjson variable "$variable" '.fields[2].int=$variable' ../data/queue/queue-datum.json > ../data/queue/queue-datum-new.json
+# update bundle size
+variable=${bundleSize}; jq --argjson variable "$variable" '.fields[1].int=$variable' ../data/queue/queue-datum.json > ../data/queue/queue-datum-new.json
 mv ../data/queue/queue-datum-new.json ../data/queue/queue-datum.json
-
 # update token info
 bundle_pid=$(jq -r '.fields[1].fields[0].bytes' ../data/sale/sale-datum.json)
 bundle_tkn=$(jq -r '.fields[1].fields[1].bytes' ../data/sale/sale-datum.json)
-variable=${bundle_pid}; jq --arg variable "$variable" '.fields[1].fields[0].bytes=$variable' ../data/queue/queue-datum.json > ../data/queue/queue-datum-new.json
-mv ../data/queue/queue-datum-new.json ../data/queue/queue-datum.json
-variable=${bundle_tkn}; jq --arg variable "$variable" '.fields[1].fields[1].bytes=$variable' ../data/queue/queue-datum.json > ../data/queue/queue-datum-new.json
-mv ../data/queue/queue-datum-new.json ../data/queue/queue-datum.json
+
 # point to a sale
 pointer_tkn=$(cat ../tmp/pointer.token)
-variable=${pointer_tkn}; jq --arg variable "$variable" '.fields[4].bytes=$variable' ../data/queue/queue-datum.json > ../data/queue/queue-datum-new.json
+
+variable=${pointer_tkn}; jq --arg variable "$variable" '.fields[3].bytes=$variable' ../data/queue/queue-datum.json > ../data/queue/queue-datum-new.json
 mv ../data/queue/queue-datum-new.json ../data/queue/queue-datum.json
 
-#
-buyer_assets=$(python3 -c "import sys; sys.path.append('../py/'); from convertMapToOutput import get_map; get_map($(jq -r '.fields[2].map' ../data/sale/sale-datum.json), ${number_of_bundles})")
-# echo $buyer_assets
-# the pure ada part if it exists
-pSize=$(jq '.fields[2].map[] | select(.k.bytes == "") | .v.map[].v.int' ../data/sale/sale-datum.json)
-if [[ -z $pSize ]]; then
-  pSize=0
+# hardcode for now
+incentive="2000000 769c4c6e9bc3ba5406b9b89fb7beb6819e638ff2e2de63f008d5bcff.744e45574d"
+
+feed_pkh=$(jq -r ' .feedHash' ../../config.json)
+feed_addr=$(../bech32 addr_test <<< 70${feed_pkh})
+feed_pid=$(jq -r ' .feedPid' ../../config.json)
+feed_tkn=$(jq -r '.feedTkn' ../../config.json)
+
+echo -e "\033[0;36m Gathering Script UTxO Information  \033[0m"
+${cli} query utxo \
+    --address ${feed_addr} \
+    ${network} \
+    --out-file ../tmp/feed_utxo.json
+TXNS=$(jq length ../tmp/feed_utxo.json)
+if [ "${TXNS}" -eq "0" ]; then
+   echo -e "\n \033[0;31m NO UTxOs Found At ${feed_addr} \033[0m \n";
+   exit;
 fi
-payAmt=$((${number_of_bundles} * ${pSize}))
+alltxin=""
+TXIN=$(jq -r --arg alltxin "" --arg policy_id "$feed_pid" --arg token_name "$feed_tkn" 'to_entries[] | select(.value.value[$policy_id][$token_name] == 1) | .key | . + $alltxin + " --tx-in"' ../tmp/feed_utxo.json)
+feed_tx_in=${TXIN::-8}
+echo Feed UTxO: $feed_tx_in
 
-incentive=" + 1000000 698a6ea0ca99f315034072af31eaac6ec11fe8558d3f48e9775aab9d.7444524950"
+feed_datum=$(jq -r --arg policy_id "$feed_pid" --arg token_name "$feed_tkn" 'to_entries[] | select(.value.value[$policy_id][$token_name] == 1) | .value.inlineDatum' ../tmp/feed_utxo.json)
+current_price=$(echo $feed_datum | jq -r '.fields[0].fields[0].map[0].v.int')
+echo Current NEWM/USD Price: $current_price
 
-queue_value="${buyer_assets}${incentive}"
+margin=$(jq '.fields[7].fields[5].int' ../data/reference/reference-datum.json)
+profit_pid=$(jq -r '.fields[7].fields[3].bytes' ../data/reference/reference-datum.json)
+profit_tkn=$(jq -r '.fields[7].fields[4].bytes' ../data/reference/reference-datum.json)
 
-# 1 drip as incentive
-# number of bundles of the cost value
+# This must be true: e < N*C + P
+profit_amt=$(python -c "p = ${margin} // ${current_price};print(p)")
+echo Profit Amt: ${profit_amt}
+profit="${profit_amt} ${profit_pid}.${profit_tkn}"
+
+# the cost part
+cost_pid=$(jq -r '.fields[2].fields[0].bytes' ../data/sale/sale-datum.json)
+
+if [ "$cost_pid" = "555344" ]; then
+    usd_amt=$(jq -r '.fields[2].fields[2].int' ../data/sale/sale-datum.json)
+    cost_amt=$(python -c "p = ${usd_amt} // ${current_price};print(p)")
+    pay_amt=$((${bundleSize} * ${cost_amt}))
+    cost="${pay_amt} ${profit_pid}.${profit_tkn}"
+else
+    cost_tkn=$(jq -r '.fields[2].fields[1].bytes' ../data/sale/sale-datum.json)
+    cost_amt=$(jq -r '.fields[2].fields[2].int' ../data/sale/sale-datum.json)
+    pay_amt=$((${bundleSize} * ${cost_amt}))
+    cost="${pay_amt} ${cost_pid}.${cost_tkn}"
+fi
+
+echo Cost: ${pay_amt}
+
+# exit
+
+extra_pct=40 # 100 / 40 = 2.5% change
+extra_amt=$(python -c "nc = ${pay_amt};p = ${profit_amt};e = (nc + p)//${extra_pct}; print(e)")
+echo Extra Amt: ${extra_amt}
+extra="${extra_amt} ${profit_pid}.${profit_tkn}"
 
 # this pays for the fees
 pub=$(jq '.fields[4].fields[0].int' ../data/reference/reference-datum.json)
 rub=$(jq '.fields[4].fields[1].int' ../data/reference/reference-datum.json)
 gas=$((${pub} + ${rub}))
-# echo "Maximum Gas Fee:" $gas
+echo "Maximum Gas Fee:" $gas
 
+# cost value + incentive + bundle
+worst_case_token="9223372036854775807 015d83f25700c83d708fbf8ad57783dc257b01a932ffceac9dcd0c3d.015d83f25700c83d708fbf8ad57783dc257b01a932ffceac9dcd0c3d00000000
++ 9223372036854775807 115d83f25700c83d708fbf8ad57783dc257b01a932ffceac9dcd0c3d.015d83f25700c83d708fbf8ad57783dc257b01a932ffceac9dcd0c3d00000000
++ 9223372036854775807 215d83f25700c83d708fbf8ad57783dc257b01a932ffceac9dcd0c3d.015d83f25700c83d708fbf8ad57783dc257b01a932ffceac9dcd0c3d00000000
+"
 min_utxo_value=$(${cli} transaction calculate-min-required-utxo \
-    --babbage-era \
-    --protocol-params-file ../tmp/protocol.json \
-    --tx-out-inline-datum-file ../data/queue/queue-datum.json \
-    --tx-out="${script_address} + 5000000 + ${queue_value}" | tr -dc '0-9')
-
-adaPay=$((${min_utxo_value} + ${payAmt} + ${gas}))
-script_address_out="${script_address} + ${adaPay} + ${queue_value}"
-
-# lovelace check
-if [[ ${1} -eq 0 ]] ; then
-    echo -e "\033[0;36m Gathering Issuer UTxO Information  \033[0m"
-    ${cli} query utxo \
-        ${network} \
-        --address ${issuer_address} \
-        --out-file ../tmp/issuer_utxo.json
-    TXNS=$(jq length ../tmp/issuer_utxo.json)
-    if [ "${TXNS}" -eq "0" ]; then
-    echo -e "\n \033[0;31m NO UTxOs Found At ${issuer_address} \033[0m \n";
-    exit;
-    fi
-    alltxin=""
-    TXIN=$(jq -r --arg alltxin "" 'keys[] | . + $alltxin + " --tx-in"' ../tmp/issuer_utxo.json)
-    issuer_tx_in=${TXIN::-8}
-
-    data="../tmp/issuer_utxo.json"
-    lovelace=$(jq '[.[] | .value."lovelace"] | add' ${data})
-    # echo "Lovelace: " ${lovelace}
-
-
-    assets=$(python3 -c "import sys; sys.path.append('../../lib/py/'); from getAllTokens import concatenate_values; concatenate_values('${data}')")
-    # echo Assets $assets
-    if [ -z "$assets" ]; then  # check if the result is an empty string
-        echo "Result is empty. Exiting."
-        # exit 1  # exit the script with an error code
-    fi
-
-    bundle_size=$(jq '.fields[2].map[0].v.map[0].v.int' ../data/sale/sale-datum.json)
-    bundle_amount=$((${number_of_bundles} * ${bundle_size}))
-    # echo "bundle amount" $bundle_amount
-    # 
-    assets=$(python3 -c "import sys; sys.path.append('../../lib/py/'); from subtract_value_string import subtract_value; subtract_value('${assets}', ${bundle_amount}, 1000000)")
-    # echo otehr assets $assets
-    counter=0
-else
-    lovelace=${1}
-    assets=${2}
-    issuer_tx_in=${3}
-    counter=${4}
-
-    if [[ ${counter} -gt ${MAX_NUMBER_OF_TX} ]] ; then
-        exit
-    fi
-
-    echo $lovelace
-    echo $issuer_tx_in
-    echo "HERE IS THE ASSETS" $assets
-
-    bundle_size=$(jq '.fields[2].map[0].v.map[0].v.int' ../data/sale/sale-datum.json)
-    bundle_amount=$((${number_of_bundles} * ${bundle_size}))
-    assets=$(python3 -c "import sys; sys.path.append('../../lib/py/'); from subtract_value_string import subtract_value; subtract_value('${assets}', ${bundle_amount}, 1000000)")
-    echo "HERE IS THE ASSETS" $assets
-    # exit
-fi
-
-adaPay=$((${lovelace} - ${adaPay}))
-
-issuer_address_out="${issuer_address} + ${adaPay} + ${assets}"
-
+        --babbage-era \
+        --protocol-params-file ../tmp/protocol.json \
+        --tx-out-inline-datum-file ../data/queue/queue-datum.json \
+        --tx-out="${script_address} + 5000000 + ${worst_case_token}" | tr -dc '0-9')
+adaPay=$((${min_utxo_value} + ${gas}))
+script_address_out="${script_address} + ${adaPay} + ${cost} + ${incentive} + ${profit} + ${extra}"
+total_newm=$((${pay_amt} + 2000000 + ${profit_amt} + ${extra_amt}))
+echo $total_newm
 echo "Script OUTPUT: "${script_address_out}
-echo "Issuer OUTPUT: "${issuer_address_out}
 #
 # exit
 #
+echo -e "\033[0;36m Gathering Buyer UTxO Information  \033[0m"
+buyer_tx_in=${1}
+buyer_lovelace=${2}
+buyer_newm=${3}
+
+fee=200000
+return_lovelace=$((${buyer_lovelace} - ${fee} - ${adaPay}))
+return_newm=$((${buyer_newm} - ${total_newm}))
+buyer_address_out="${buyer_address} + ${return_lovelace} + ${return_newm} 769c4c6e9bc3ba5406b9b89fb7beb6819e638ff2e2de63f008d5bcff.744e45574d"
+
+
+# exit
 echo -e "\033[0;36m Building Tx \033[0m"
 ${cli} transaction build-raw \
     --babbage-era \
     --protocol-params-file ../tmp/protocol.json \
     --out-file ../tmp/tx.draft \
-    --tx-in ${issuer_tx_in} \
+    --tx-in ${buyer_tx_in} \
     --tx-out="${script_address_out}" \
     --tx-out-inline-datum-file ../data/queue/queue-datum.json  \
-    --tx-out="${issuer_address_out}" \
-    --fee 900000
-
-FEE=$(${cli} transaction calculate-min-fee --tx-body-file ../tmp/tx.draft ${network} --protocol-params-file ../tmp/protocol.json --tx-in-count 1 --tx-out-count 2 --witness-count 1)
-# echo $FEE
-fee=$(echo $FEE | rev | cut -c 9- | rev)
-
-lovelace_return=$((${adaPay} - ${fee}))
-issuer_address_out="${issuer_address} + ${lovelace_return} + ${assets}"
-
-
-${cli} transaction build-raw \
-    --babbage-era \
-    --protocol-params-file ../tmp/protocol.json \
-    --out-file ../tmp/tx.draft \
-    --tx-in ${issuer_tx_in} \
-    --tx-out="${script_address_out}" \
-    --tx-out-inline-datum-file ../data/queue/queue-datum.json  \
-    --tx-out="${issuer_address_out}" \
+    --tx-out="${buyer_address_out}" \
     --fee ${fee}
+
+echo -e "\033[1;32m Fee: \033[0m" $fee
 #
 # exit
 #
 echo -e "\033[0;36m Signing \033[0m"
 ${cli} transaction sign \
-    --signing-key-file ../wallets/${issuer_path}/payment.skey \
+    --signing-key-file ../wallets/${buyer_path}/payment.skey \
     --tx-body-file ../tmp/tx.draft \
     --out-file ../tmp/tx.signed \
     ${network}
@@ -188,6 +165,5 @@ ${cli} transaction submit \
 tx=$(${cli} transaction txid --tx-file ../tmp/tx.signed)
 echo "Tx Hash:" $tx
 
-python3 -c "import time, random; time.sleep(random.uniform(0, 10))"
-increment=$((${counter} + 1))
-./05_autoPopulateQueue.sh ${lovelace_return} "${assets}" "${tx}#1" ${increment}
+python3 -c "import time, random; time.sleep(random.uniform(0, 2))"
+./05_autoPopulateQueue.sh ${tx}#1 $return_lovelace $return_newm
