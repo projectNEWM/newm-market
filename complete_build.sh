b2e65db64a7271b5f35cd757c460a4c9366728e3
logicalmechanism <logical.mechanism@protonmail.com>
2024-09-03 13:43:49 -0700

    queue and pointer done

--- complete_build.sh
+++ complete_build.sh
@@ -64,19 +64,19 @@ aiken blueprint apply -o plutus.json -v sale.contract.spend "${ref_cbor}"
 aiken blueprint convert -v sale.contract.spend > contracts/sale_contract.plutus
 cardano-cli transaction policyid --script-file contracts/sale_contract.plutus > hashes/sale.hash
 
-# echo -e "\033[1;33m Convert Queue Contract \033[0m"
-# aiken blueprint apply -o plutus.json -v queue.params "${pid_cbor}"
-# aiken blueprint apply -o plutus.json -v queue.params "${tkn_cbor}"
-# aiken blueprint apply -o plutus.json -v queue.params "${ref_cbor}"
-# aiken blueprint convert -v queue.params > contracts/queue_contract.plutus
-# cardano-cli transaction policyid --script-file contracts/queue_contract.plutus > hashes/queue.hash
+echo -e "\033[1;33m Convert Queue Contract \033[0m"
+aiken blueprint apply -o plutus.json -v queue.contract.spend "${pid_cbor}"
+aiken blueprint apply -o plutus.json -v queue.contract.spend "${tkn_cbor}"
+aiken blueprint apply -o plutus.json -v queue.contract.spend "${ref_cbor}"
+aiken blueprint convert -v queue.contract.spend > contracts/queue_contract.plutus
+cardano-cli transaction policyid --script-file contracts/queue_contract.plutus > hashes/queue.hash
 
-# echo -e "\033[1;33m Convert Pointer Contract \033[0m"
-# aiken blueprint apply -o plutus.json -v pointer.params "${pid_cbor}"
-# aiken blueprint apply -o plutus.json -v pointer.params "${tkn_cbor}"
-# aiken blueprint apply -o plutus.json -v pointer.params "${ref_cbor}"
-# aiken blueprint convert -v pointer.params > contracts/pointer_contract.plutus
-# cardano-cli transaction policyid --script-file contracts/pointer_contract.plutus > hashes/pointer_policy.hash
+echo -e "\033[1;33m Convert Pointer Contract \033[0m"
+aiken blueprint apply -o plutus.json -v pointer.contract.mint "${pid_cbor}"
+aiken blueprint apply -o plutus.json -v pointer.contract.mint "${tkn_cbor}"
+aiken blueprint apply -o plutus.json -v pointer.contract.mint "${ref_cbor}"
+aiken blueprint convert -v pointer.contract.mint > contracts/pointer_contract.plutus
+cardano-cli transaction policyid --script-file contracts/pointer_contract.plutus > hashes/pointer_policy.hash
 
 # echo -e "\033[1;33m Convert Band Lock Contract \033[0m"
 # aiken blueprint apply -o plutus.json -v band.params "${pid_cbor}"
